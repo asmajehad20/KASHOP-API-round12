15dd8aae96d0083243bc519c62734ca5bc7a95b9
asmajehad20 <asmajehad919@gmail.com>
2025-11-19 16:55:10 +0200

    initial

---
+++ KSHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using KSHOP.DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KSHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoriesController(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+    }
+}
