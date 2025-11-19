cffd07f5c50d7d31accfc48557904cb85ab908fb
asmajehad20 <asmajehad919@gmail.com>
2025-11-19 16:55:10 +0200

    initial

---
+++ KSHOP.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using KSHOP.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSHOP.DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+
+}
